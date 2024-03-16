017df1903ea8852c77e45c5c172d8e0c73e9514a
yousefse3a <62356610+yousefse3a@users.noreply.github.com>
2024-03-16 13:52:37 +0200

    add about joinus ... sections

--- src/App.jsx
+++ src/App.jsx
@@ -17,22 +17,12 @@ import Sponsers from "./sections/Sponsers";
 import JoinUs from "./sections/JoinUs";
 
 function App() {
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-  console.log("seee", screenWidth);
-  const cacheRtl = createCache({
-    key: "muirtl",
-    stylisPlugins: [prefixer, stylisRTLPlugin],
-  });
-  useEffect(() => {
-    console.log("screenWidth", screenWidth);
-  }, [screenWidth]);
-
   return (
     <>
       <Grid
         sx={{
           overflow: "hidden",
-          minWidth: "98vw",
+          minWidth: "100vw",
         }}
       >
         <Navber />
@@ -44,7 +34,6 @@ function App() {
         <Sponsers />
         <JoinUs />
       </Grid>
-
     </>
   );
 }
