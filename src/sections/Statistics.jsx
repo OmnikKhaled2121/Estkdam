81aaa4212a72a35d9feaa06fa1e7fafd0b71aa08
Omnia Khaled <83430755+OmnikKhaled2121@users.noreply.github.com>
2024-03-30 02:37:12 +0200

    Common question

--- src/sections/Statistics.jsx
+++ src/sections/Statistics.jsx
@@ -1,35 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Container, Grid, Typography } from "@mui/material";
+import axios from "axios";
 
-const Data = [
-  {
-    title: "أكثر من ",
-    num: "12000",
-    desc: "طلب",
-  },
-  {
-    title: "أكثر من ",
-    num: "6500",
-    desc: "عقد ناجح",
-  },
-  {
-    title: "أكثر من ",
-    num: "120",
-    desc: "مكتب استقدام",
-  },
-  {
-    title: "أكثر من ",
-    num: "10",
-    desc: "جنسيات",
-  },
-];
+
+
+
 
 export default function Statistics() {
+  const [number, setNumber] = useState([])
+
+  async function DynamicData() {
+    let { data } = await axios.get(`https://estikdam.jacadix.net/api/statistics`)
+    setNumber(data.data[0])
+    console.log("number", number)
+    return number
+
+  }
+
+  useEffect(() => {
+    DynamicData()
+  }, [])
+
   return (
-
-
-    <Grid sx={{ background: "#DAEAF4",
-    marginBottom:"6rem"}}>
+    <Grid sx={{
+      background: "#DAEAF4",
+      marginBottom: "6rem"
+    }}>
       <Container>
         <Grid container >
           <Grid
@@ -42,14 +38,147 @@ export default function Statistics() {
               padding: "3rem 0",
             }}
           >
-            {Data.map((item) => {
+
+            <Grid xs={3}
+              sx={{
+                flexDirection: "column",
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                color: "#005288",
+                fontFamily: "Almarai",
+                textAlign: "center",
+                fontWeight: 700,
+              }}
+            >
+              <Box
+                component="span"
+                sx={{ fontWeight: "400", fontSize: "20px" }}
+              >
+                أكثر من
+              </Box>
+              <Box
+                component="span"
+                sx={{ fontWeight: "800", fontSize: "48px", paddingTop: "1rem" }}
+              >
+                { number? number.orders:""}
+              </Box>
+              <Box
+                component="span"
+                sx={{ fontWeight: "700", fontSize: "36px" }}
+              >
+                طلب
+              </Box>
+            </Grid>
+
+            <Grid xs={3}
+              sx={{
+                flexDirection: "column",
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                color: "#005288",
+                fontFamily: "Almarai",
+                textAlign: "center",
+                fontWeight: 700,
+              }}
+            >
+              <Box
+                component="span"
+                sx={{ fontWeight: "400", fontSize: "20px" }}
+              >
+                أكثر من
+              </Box>
+              <Box
+                component="span"
+                sx={{ fontWeight: "800", fontSize: "48px", paddingTop: "1rem" }}
+              >
+                { number? number.contracts:""}
+              </Box>
+              <Box
+                component="span"
+                sx={{ fontWeight: "700", fontSize: "36px" }}
+              >
+                عقد ناجح
+              </Box>
+            </Grid>
+
+            <Grid xs={3}
+              sx={{
+                flexDirection: "column",
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                color: "#005288",
+                fontFamily: "Almarai",
+                textAlign: "center",
+                fontWeight: 700,
+              }}
+            >
+              <Box
+                component="span"
+                sx={{ fontWeight: "400", fontSize: "20px" }}
+              >
+                أكثر من
+              </Box>
+              <Box
+                component="span"
+                sx={{ fontWeight: "800", fontSize: "48px", paddingTop: "1rem" }}
+              >
+                {number.offices}
+              </Box>
+              <Box
+                component="span"
+                sx={{ fontWeight: "700", fontSize: "36px" }}
+              >
+                مكتب استقدام
+              </Box>
+            </Grid>
+
+            <Grid xs={3}
+              sx={{
+                flexDirection: "column",
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                color: "#005288",
+                fontFamily: "Almarai",
+                textAlign: "center",
+                fontWeight: 700,
+              }}
+            >
+              <Box
+                component="span"
+                sx={{ fontWeight: "400", fontSize: "20px" }}
+              >
+                أكثر من
+              </Box>
+              <Box
+                component="span"
+                sx={{ fontWeight: "800", fontSize: "48px", paddingTop: "1rem" }}
+              >
+                {number.nationalities}
+              </Box>
+              <Box
+                component="span"
+                sx={{ fontWeight: "700", fontSize: "36px" }}
+              >
+                جنسيات
+              </Box>
+            </Grid>
+
+
+
+
+
+            {/* {Data.map((item) => {
+              console.log("item", item)
               return (
                 <>
                   <Grid xs={3}
                     sx={{
                       flexDirection: "column",
                       display: "flex",
-                      // padding: "0 5rem",
                       justifyContent: "space-between",
                       alignItems: "center",
                       color: "#005288",
@@ -79,7 +208,7 @@ export default function Statistics() {
                   </Grid>
                 </>
               );
-            })}
+            })} */}
           </Grid>
 
         </Grid>
